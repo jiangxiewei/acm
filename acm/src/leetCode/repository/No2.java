4b699b0efce4a5f7167f7fb4f37fa481af838dd4
jiangxiewei <jiangxiewei@qq.com>
2021-10-26 02:26:38 +0800

    :art: 重做No2题,实现更优雅

--- acm/src/leetCode/repository/No2.java
+++ acm/src/leetCode/repository/No2.java
@@ -1,83 +1,116 @@
 package leetCode.repository;
 
 
+import java.util.function.BiFunction;
+
+/**
+ * https://leetcode-cn.com/problems/add-two-numbers/submissions/
+ */
 public class No2 {
 
-
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode targetHead = new ListNode(0), target = targetHead;
-        boolean go = false;
-        while (l1 != null || l2 != null) {
-            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + (go ? 1 : 0);
-            go = false;
-            if (sum >= 10) {
-                sum -= 10;
-                go = true;
-            }
-            target.next = new ListNode(sum);
-            target = target.next;
-            l1 = l1 != null ? l1.next : null;
-            l2 = l2 != null ? l2.next : null;
-        }
-        if (go) {
-            target.next = new ListNode(1);
-        }
-
-        return targetHead.next;
-    }
-
     /**
-     * 链表倒置
-     *
-     * @return 倒置后的新head节点
+     * 默认数据结构
      */
-    private ListNode reserveList(ListNode headNode) {
-        ListNode cur = headNode, pre = null, next = null;
-        while (cur != null) {
-            next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return pre;
-    }
-
-    private static class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
-        ListNode(int x) {
-            val = x;
+        ListNode(int val) {
+            this.val = val;
         }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        /**
+         * 自己加的,打印用的
+         */
+        public void print(){
+            System.out.print(val);
+            if (next != null) {
+                next.print();
+            }
+        }
+
     }
 
+    /**
+     * 测试用main方法
+     * @param args
+     */
     public static void main(String[] args) {
-        int[] l1 = new int[]{2, 4, 3}, l2 = new int[]{5, 6, 4};
-        ListNode head1 = new ListNode(l1[0]), pos = head1;
-        for (int i = 1; i < l1.length; i++) {
-            pos.next = new ListNode(l1[i]);
-            pos = pos.next;
-        }
-
-        ListNode head2 = new ListNode(l2[0]);
-        pos = head2;
-        for (int i = 1; i < l2.length; i++) {
-            pos.next = new ListNode(l2[i]);
-            pos = pos.next;
-        }
-
-        System.out.println(listTostring(new No2().addTwoNumbers(head1, head2)));
+        test(new ListNode(2, new ListNode(4, new ListNode(3))),
+                new ListNode(5, new ListNode(6, new ListNode(4))));
+        test(new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
+                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))));
     }
 
-    public static String listTostring(ListNode listNode) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        ListNode node = listNode;
-        while (node.next != null) {
-            sb.append(node.val).append(",");
-            node = node.next;
-        }
-        sb.append(node.val + "]");
-        return sb.toString();
+    
+    public static void test(ListNode l1, ListNode l2) {
+        l1.print();
+        System.out.print("+");
+        l2.print();
+        System.out.print("=");
+        new No2().addTwoNumbers(l1, l2).print();
+        System.out.println();
     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return new NewWay().apply(l1, l2);
+    }
+
+    /**
+     * 新写的方法,看起来更优雅点
+     */
+    public static class NewWay implements BiFunction<ListNode, ListNode, ListNode> {
+
+        @Override
+        public ListNode apply(ListNode l1, ListNode l2) {
+            return addTwoAndEnter(l1, l2, 0);
+        }
+
+        private ListNode addTwoAndEnter(ListNode l1, ListNode l2, int inc) {
+            if (l1 == null && l2 == null && inc == 0) {
+                return null;
+            }
+            int v1 = l1 != null ? l1.val : 0;
+            int v2 = l2 != null ? l2.val : 0;
+            ListNode n1 = l1 != null ? l1.next : null;
+            ListNode n2 = l2 != null ? l2.next : null;
+            int newv = (v1 + v2 + inc);
+            return new ListNode(newv % 10, addTwoAndEnter(n1, n2, newv / 10));
+        }
+    }
+    
+    /**
+     * 很久以前写的逻辑. 归档至此
+     */
+    public static class OldWay implements BiFunction<ListNode,ListNode,ListNode> {
+
+        @Override
+        public ListNode apply(ListNode l1, ListNode l2) {
+            ListNode targetHead = new ListNode(0), target = targetHead;
+            boolean go = false;
+            while (l1 != null || l2 != null) {
+                int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + (go ? 1 : 0);
+                go = false;
+                if (sum >= 10) {
+                    sum -= 10;
+                    go = true;
+                }
+                target.next = new ListNode(sum);
+                target = target.next;
+                l1 = l1 != null ? l1.next : null;
+                l2 = l2 != null ? l2.next : null;
+            }
+            if (go) {
+                target.next = new ListNode(1);
+            }
+
+            return targetHead.next;
+        }
+    }
+
 }
