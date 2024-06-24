1f3ef41763d545655f0882bc6a515a072a22925c
J <jiangxiewei@qq.com>
2024-06-25 00:52:39 +0800

    No.5 增加dp法，优化一部分代码。

--- rust/src/leetcode/no_5.rs
+++ rust/src/leetcode/no_5.rs
@@ -1,3 +1,4 @@
+
 struct Solution {}
 
 /** 
@@ -6,56 +7,76 @@ struct Solution {}
 impl Solution {
     // 最长回文子串
     pub fn longest_palindrome(s: String) -> String {
-        Self::dynamic_programing(s)
+        Self::spread_from_center(s)
     }
     
     
     /**
-        动态规划法
-     **/
-    fn dynamic_programing(s: String) -> String {
-        let arr = s.as_bytes();
+        **动态规划法**
+        dp\[i\]\[j\] = dp\[i+1\]\[j-1\] && s\[i\] == s\[j\]
+     */
+    fn dynamic_programing(str: String) -> String {
+        let arr = str.as_bytes();
         let arr_len: usize = arr.len();
-        let mut dp = vec![vec![0u32; arr_len]; arr_len];
+        let mut dp = vec![vec![false; arr_len]; arr_len];
+
+        let (mut ts, mut te, mut max) = (0usize, 0usize, 1);
+
         for i in 0..arr_len {
-            dp[i][i] = 1;
+            dp[i][i] = true;
+            if i+1 < arr_len {
+                dp[i][i + 1] = arr[i] == arr[i + 1];
+                if dp[i][i + 1] && 2 > max { (ts, te, max) = (i, i + 1, 2) };
+            }
         }
-        
-        // todo 
-        
-        
-        "".to_string()
+
+        for i in 0..arr_len-1 {
+            let (mut s, mut e) = (0, 2 + i);
+            while s < arr_len - 1 && e < arr_len {
+                if dp[s + 1][e - 1] && arr[s] == arr[e] { dp[s][e] = true; };
+                if dp[s][e] && e - s + 1 > max { (ts, te, max) = (s, e, e - s + 1); };
+                (s, e) = (s + 1, e + 1);
+            };
+        }
+        str[ts..=te].to_string()
     }
     
     /**
         中心扩散法
     **/
     fn spread_from_center(s: String) -> String {
-        let arr = s.as_bytes();
-        let (mut cl, mut cr, mut max_length) = (0usize, 0usize, 0usize);
-        for i in 0..arr.len() {
-            let (l, r) = Self::spread_from(arr, (i, i));
-            (cl, cr, max_length) = if r - l + 1 > max_length { (l, r, r - l + 1) } else { (cl, cr, max_length) };
-            if i + 1 < arr.len() {
-                let (l, r) = Self::spread_from(arr, (i, i + 1));
-                (cl, cr, max_length) = if r - l + 1 > max_length { (l, r, r - l + 1) } else { (cl, cr, max_length) };
-            }
+        let arr = s.into_bytes();
+        let mut result: &[u8] = &[arr[0]];
+        for i in 0..arr.len()-1 {
+            let (sub1, sub2) = (
+                Self::spread_from(&arr, (i, i)),
+                Self::spread_from(&arr, (i, (i + 1)))
+            );
+            result = if sub1.len() <= result.len() && sub2.len() <= result.len() {
+                result
+            } else if sub1.len() > sub2.len() {
+                sub1
+            } else {
+                sub2
+            };
+        };
+        unsafe {
+            String::from_utf8_unchecked(result.into())
         }
-        s[cl..=cr].to_string()
     }
 
-    fn spread_from(arr: &[u8], (l, r): (usize, usize)) -> (usize, usize) {
-        let (mut tl, mut tr) = (l as i32, r as i32);
-        while tl >= 0 && tr < arr.len() as i32 && arr[tl as usize] == arr[tr as usize] {
-            tl -= 1;
+    fn spread_from(arr: &Vec<u8>, (mut tl,mut tr): (usize,usize)) -> &[u8] {
+        while tl < arr.len() && tr < arr.len() && arr[tl] == arr[tr] {
+            tl = tl.wrapping_add_signed(-1);
             tr += 1;
         };
-        if (tl + 1) <= (tr - 1) {
-            ((tl + 1) as usize, (tr - 1) as usize)
+        if (tl.wrapping_add_signed(1)) <= (tr - 1) {
+            &arr[tl.wrapping_add_signed(1)..=tr - 1]
         } else {
-            (tl as usize, tl as usize)
+            &arr[tl..tl]
         }
     }
+
 }
 
 
@@ -65,4 +86,7 @@ fn test() {
     assert_eq!("bb", dbg!(Solution::longest_palindrome("cbbd".to_string())));
     assert_eq!("ccc", dbg!(Solution::longest_palindrome("ccc".to_string())));
     assert_eq!("a", dbg!(Solution::longest_palindrome("a".to_string())));
+    assert_eq!("aaaa", dbg!(Solution::longest_palindrome("aaaa".to_string())));
+    assert_eq!("aca", dbg!(Solution::longest_palindrome("aacabdkacaa".to_string())));
+
 }
